e6e2a1c8a6753dc685682137d8f7405a47b922ce
peaceOffiong <peaceyben@gamil.com>
2022-08-17 22:57:34 -0700

    initial commit

---
+++ pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eum quaerat repellat voluptas pariatur, sint molestias ullam officiis natus veniam alias illo repellendus laudantium necessitatibus odit suscipit, sequi ratione aliquam.
+      </p>
+    </section>
+  )
+}
+
+export default About
